d78bc2c923bb1ef00866b61b20718a7a95af8225
seoanezonjic <seoanezonjic@hotmail.com>
2016-07-20 23:01:24 +0200

    Revert "regexp_ver"
    This reverts commit 83b924f6b9225b8a520c5f4ec29cd0b162647a3d.

--- lib/autoflow.rb
+++ lib/autoflow.rb
@@ -1,4 +1,3 @@
-
 require "autoflow/version"
 require "autoflow/stack"
 
--- lib/autoflow/program.rb
+++ lib/autoflow/program.rb
@@ -1,25 +1,22 @@
 class Program
-	attr_accessor :name, :initialization, :parameters, :dependencies, :attrib, :queue_id, :batch
-
-	def initialize(name, initialization, parameters, dependencies, job_attrib)
-		@name = name
-		@initialization = initialization
-    	@parameters = parameters
-		@dependencies = dependencies
-		@attrib = job_attrib
-		@queue_id = nil
-		@batch = nil
+	attr_accessor :name, :exec_folder_program, :parameters, :initialization, :queue_id, :done, :dependencies, :monocpu, :end_tasks_buffer
+	def initialize(name, parameters, initialization, iterated, exec_folder_program, dependencies, done, end_tasks_buffer)
+		@name=name
+    		@parameters=parameters
+		@initialization=initialization
+		@exec_folder_program=exec_folder_program
+		@iterated=iterated
+		@queue_id=nil
+		@done=done
+		@dependencies=dependencies
+		@end_tasks_buffer=end_tasks_buffer
+		@monocpu=TRUE
+		if @parameters =~ /\[cpu\]/
+			@monocpu=FALSE
+		end
 	end
 
 	def inspect
-		if @parameters.class.to_s == 'String'
-			program = @parameters.split(' ').first
-			command = @parameters.gsub("\n","\n\t")
-		else
-			program = 'iterative_job'
-			command = @parameters.map{|b| b}.join(' ')
-		end
-		string="\e[31m#{program}\n\e[0m\t\e[33m#{command}\e[0m\e[34m#{@attrib[:exec_folder]}\e[0m"
+		string=@name.to_s+"\t"+@parameters.to_s+"\t"+@exec_folder_program.to_s+"\t"+"\t"+@iterated.to_s
 	end
-
 end
